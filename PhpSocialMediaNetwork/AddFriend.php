7cf47f26d05516155e450cb897eb00c6d42320cd
caparra92 <caparra92@gmail.com>
2024-12-01 23:52:42 -0500

    AddFiend.php validations done

--- PhpSocialMediaNetwork/AddFriend.php
+++ PhpSocialMediaNetwork/AddFriend.php
@@ -3,8 +3,61 @@
   include_once "EntityClassLib.php";
   // Include the external file with database functions
   include 'dbFunctions.php';
+  include_once 'ValidationFunctions.php';
   // Start session
   session_start();
+
+  extract($_POST);
+  $errors = [];
+  $messages = [];
+  if(isset($_SESSION['userId'])) {
+    $userId = $_SESSION['userId'];
+  }
+
+  if (isset($sendFriendRequest)) {
+    $friendRequesteeId = filter_input(INPUT_POST, 'friendRequesteeId', FILTER_UNSAFE_RAW);
+
+    try {
+      $userIdError = ValidateId($friendRequesteeId);
+
+      if ($userIdError) {
+        $errors['userId'] = $userIdError;
+      }
+
+      if(!getUserId($friendRequesteeId)) {
+        $errors['userId'] = 'User Id: '.$friendRequesteeId.' does not exist!';
+      }
+      
+      if(validateFriendship($userId, $friendRequesteeId)) {
+        $errors['friendship'] = 'You are already a friend of user: '.$friendRequesteeId;
+      }
+
+      if(validateFriendship($friendRequesteeId, $userId)) {
+        $errors['friendship'] = 'You are already a friend of user: '.$friendRequesteeId;
+      }
+
+      if(validateFriendshipRequest($userId, $friendRequesteeId)) {
+        $errors['friendshipRequest'] = 'You already have sent a friend request to user: '.$friendRequesteeId;
+      }
+
+      if(validateFriendshipRequest($friendRequesteeId, $userId)) {
+        addFriend($friendRequesteeId, $userId);
+        $errors['friendshipRequest'] = 'You already have a friend request from user : '.$friendRequesteeId.' you have become friends';
+      }
+
+      if($friendRequesteeId == $userId) {
+        $errors['friendshipRequest'] = 'You cannot send a friend request to yourself';
+      }
+
+      if(empty($errors)) {
+        addFriendRequest($userId, $friendRequesteeId);
+        $messages['friendshipRequest'] = 'You have successfully sent a friend request to: '.$friendRequesteeId;
+      }
+
+    } catch (Exception $ex) {
+        $errors['dbError'] = "Database error: " . $ex->getMessage();
+    }
+  }
 
 ?>
 
@@ -12,16 +65,34 @@
 
 <div class="container">
   <h2 class="mt-3">Add Friend</h2>
-  <h6 class="mb-4">Welcome  (You can change user <a href="Login.php">here</a> if this is not you.)</h6>
+  <h6 class="mb-4">Welcome <?php echo $userId ?> (You can change user <a href="Login.php">here</a> if this is not you.)</h6>
   
-      <div class="card">
-  <h6 class="card-header">Send a friend invite</h6>
-  <div class="card-body">
-    
-    <input type="text" class="form-control">
-    <p class="card-text">Enter the ID of the user you want to be friends with.</p>
-    <a href="#" class="btn" style="background-color: DarkSlateBlue; color: white;">Send Friend Request</a>
-  </div>
+  <form action="AddFriend.php" method="post">
+    <div class="card">
+      <h6 class="card-header">Send a friend invite</h6>
+      <div class="card-body">
+      
+        <input type="text" class="form-control" name="friendRequesteeId">
+        <?php if (isset($errors['userId'])): ?>
+            <span class="error text-danger"><?php echo $errors['userId']; ?></span>
+        <?php endif; ?>
+        <?php if (isset($errors['friendship'])): ?>
+            <span class="error text-danger"><?php echo $errors['friendship']; ?></span>
+        <?php endif; ?>
+        <?php if (isset($errors['friendshipRequest'])): ?>
+            <span class="error text-danger"><?php echo $errors['friendshipRequest']; ?></span>
+        <?php endif; ?>
+        <?php if (isset($messages['friendshipRequest'])): ?>
+            <span class="error text-danger"><?php echo $messages['friendshipRequest']; ?></span>
+        <?php endif; ?>
+        <?php if (isset($errors['dbError'])): ?>
+            <span class="error text-danger"><?php echo $errors['dbError']; ?></span>
+        <?php endif; ?>
+        <p class="card-text">Enter the ID of the user you want to be friends with.</p>
+        <button type="submit" name="sendFriendRequest" class="btn" style="background-color: DarkSlateBlue; color: white;">Send Friend Request</button>
+      </div>
+    </div>
+  </form>
   
 </div>
 <?php include('./common/footer.php'); ?>
--- PhpSocialMediaNetwork/dbFunctions.php
+++ PhpSocialMediaNetwork/dbFunctions.php
@@ -11,7 +11,7 @@ function validateLogin($userId, $password) {
     $pdo = getPDO();
     
     // Prepare the SQL query
-    $sql = "SELECT UserId, Name, Phone, Password FROM User WHERE UserId = :userId";
+    $sql = "SELECT UserId, Name, Phone, Password FROM user WHERE UserId = :userId";
     
     // Use prepared statements to avoid SQL injection
     $stmt = $pdo->prepare($sql);
@@ -44,7 +44,7 @@ function addNewUser($userId, $name, $phone, $password) {
         $pdo = getPDO();
         
         // Use a prepared statement for security and to avoid SQL injection
-        $sql = "INSERT INTO User (UserId, Name, Phone, Password) VALUES (:userId, :name, :phone, :password)";
+        $sql = "INSERT INTO user (UserId, Name, Phone, Password) VALUES (:userId, :name, :phone, :password)";
         $stmt = $pdo->prepare($sql);
         
         // Bind the parameters
@@ -62,6 +62,95 @@ function addNewUser($userId, $name, $phone, $password) {
         } else {
             throw new Exception("Database error: " . $e->getMessage());
         }
+    }
+}
+
+function getUserId($userId) {
+    try {
+        $pdo = getPDO();
+        $sql = "SELECT UserId FROM user WHERE UserId = :userId";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':userId', $userId);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $studentExists = $row ? true : false;
+        return $studentExists;
+    } catch (PDOException $ex) {
+        die("Database error: " . $ex->getMessage());
+    }
+}
+
+function validateFriendship($friendRequesterId, $friendRequesteeId) {
+    try {
+        $pdo = getPDO();
+        $sql = "SELECT Status FROM friendship WHERE Friend_RequesterId = :friendRequesterId AND Friend_RequesteeId = :friendRequesteeId";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':friendRequesterId', $friendRequesterId);
+        $stmt->bindParam(':friendRequesteeId',$friendRequesteeId);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($row) {
+            $friendshipExists = $row['Status'] == 'accepted' ? true : false;
+            return $friendshipExists;
+        } else {
+            return false;
+        }
+    } catch (PDOException $ex) {
+        die("Database error: " . $ex->getMessage());
+    }
+}
+
+
+function validateFriendshipRequest($friendRequesterId,$friendRequesteeId) {
+    try {
+        $pdo = getPDO();
+        $sql = "SELECT Friend_RequesterId FROM friendship "
+                ."WHERE Friend_RequesteeId = :friendRequesteeId AND Status = 'request'";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':friendRequesteeId',$friendRequesteeId);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($row) {
+            $friendshipRequestExists = $row['Friend_RequesterId'] == $friendRequesterId ? true : false;
+            return $friendshipRequestExists;
+        } else {
+            return false;
+        }
+    } catch (PDOException $ex) {
+        die("Database error: " . $ex->getMessage());
+    }
+}
+
+function addFriendRequest($friendRequesterId, $friendRequesteeId) {
+    try {
+        $pdo = getPDO();
+        
+        if(getUserId($friendRequesteeId)) {
+            $sql = "INSERT INTO friendship (Friend_RequesterId, Friend_RequesteeId, Status) VALUES (:friendRequesterId, :friendRequesteeId, 'request')";
+            $stmt = $pdo->prepare($sql);
+            
+            $stmt->bindParam(':friendRequesterId', $friendRequesterId);
+            $stmt->bindParam(':friendRequesteeId', $friendRequesteeId);
+
+            $stmt->execute();
+        }
+        
+    } catch (PDOException $e) {
+        
+        throw new Exception("Database error: " . $e->getMessage());
+    }
+}
+
+function addFriend($friendRequesterId, $friendRequesteeId) {
+    try {
+        $pdo = getPDO();
+        $sql = "UPDATE friendship SET status = 'accepted' WHERE Friend_RequesterId = :friendRequesterId AND Friend_RequesteeId = :friendRequesteeId";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':friendRequesterId', $friendRequesterId);
+        $stmt->bindParam(':friendRequesteeId', $friendRequesteeId);
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        die("Database error: " . $ex->getMessage());
     }
 }
 ?>
