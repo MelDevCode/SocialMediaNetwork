0e99b02bf9093f80f2f8337cd5d1bca11c5157c3
caparra92 <caparra92@gmail.com>
2024-12-02 19:47:50 -0500

    Update friend functions

--- PhpSocialMediaNetwork/dbFunctions.php
+++ PhpSocialMediaNetwork/dbFunctions.php
@@ -153,4 +153,71 @@ function addFriend($friendRequesterId, $friendRequesteeId) {
         die("Database error: " . $ex->getMessage());
     }
 }
+
+function acceptFriendRequest($userId, $requesterId) {
+    try {
+        $pdo = getPDO();
+        $sql = "UPDATE Friendship SET Status = 'accepted' WHERE Friend_RequesterId = :requesterId AND Friend_RequesteeId = :userId";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':requesterId', $requesterId);
+        $stmt->bindParam(':userId', $userId);
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        die("Database error: " . $ex->getMessage());
+    }
+}
+
+function getFriendRequesters($userId) {
+    try {
+        $pdo = getPDO();
+        $sql = "SELECT Friend_RequesterId FROM friendship "
+                . "WHERE Friend_RequesteeId = :userId AND Status = 'request'";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':userId', $userId);
+        $stmt->execute();
+        $friendRequesters = array();
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $friendRequesters[] = $row["Friend_RequesterId"];
+        }
+
+        return $friendRequesters;
+    } catch (PDOException $ex) {
+        die("Database error: " . $ex->getMessage());
+    }
+}
+
+function getFriends($userId) {
+    try {
+        $pdo = getPDO();
+        $sql = "SELECT Friend_RequesterId FROM friendship "
+                ."WHERE Friend_RequesteeId = :userId AND Status = 'accepted' "
+                ."UNION SELECT Friend_RequesteeId FROM friendship "
+                ."WHERE Friend_RequesterId = :userId AND Status = 'accepted' "
+                ."ORDER BY 1";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':userId', $userId);
+        $stmt->execute();
+        $friends = array();
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $friends[] = $row["Friend_RequesterId"];
+        }
+
+        return $friends;
+    } catch (PDOException $ex) {
+        die("Database error: " . $ex->getMessage());
+    }
+}
+
+function deleteFriend($userId, $friendId) {
+    try {
+        $pdo = getPDO();
+        $sql = "DELETE FROM friendship WHERE (Friend_RequesterId = :userId || Friend_RequesteeId = :userId) AND (Friend_RequesteeId = :friendId || Friend_RequesterId = :friendId)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':userId', $userId);
+        $stmt->bindParam(':friendId', $friendId);
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        die("Database error: " . $ex->getMessage());
+    }
+}
 ?>
