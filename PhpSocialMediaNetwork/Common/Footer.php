f3ccbb66f1cd59f54a34fe7115eb9860dc785e96
caparra92 <caparra92@gmail.com>
2024-12-02 19:52:34 -0500

    Update footer with js functions

--- PhpSocialMediaNetwork/Common/Footer.php
+++ PhpSocialMediaNetwork/Common/Footer.php
@@ -5,5 +5,18 @@
     </footer>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.6/dist/js/bootstrap.min.js"></script>
+    <script>
+      function confirmDeletion(e) {
+        if(!confirm("The selected friends will be defriended")) {
+            e.preventDefault();
+        }
+      }
+
+      function confirmDeny(e) {
+        if(!confirm("The selected requests will be denied")) {
+            e.preventDefault();
+        }
+      }
+    </script>
 </body>
 </html> 
