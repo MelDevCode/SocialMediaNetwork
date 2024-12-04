0fd4de6d237ec07d556811d20960fc60fccc129b
caparra92 <caparra92@gmail.com>
2024-12-03 23:34:50 -0500

    My friends done

--- PhpSocialMediaNetwork/MyFriends.php
+++ PhpSocialMediaNetwork/MyFriends.php
@@ -2,7 +2,7 @@
   // Include external entity class library
   include_once "EntityClassLib.php";
   // Include the external file with database functions
-  include 'dbFunctions.php';
+  include_once 'dbFunctions.php';
   // Start session
   session_start();
   extract($_POST);
@@ -12,6 +12,9 @@
         $user = $_SESSION['user'];
         $userName = $user['Name'];
         $userId = $user['UserId'];  // Get the student's name
+        if (!isset($_SESSION['redirect_to'])) {
+            $_SESSION['redirect_to'] = basename($_SERVER['PHP_SELF']) . '?' . http_build_query($_GET);
+        }
     } else {
         header("Location: Login.php");
         exit();
@@ -31,19 +34,30 @@
         }
     }
 
-    if(isset($acceptRequest)) {
-        if(!isset($friends)) {
-            $errors["noFriendsSelected"] = "You must select at least one friend!";
-        } else {
-            if(empty($errors)) {
-                foreach($friends as $friend) {
-                    deleteFriend($userId, $friend);
+    if(isset($submit)) {
+        if($submit == 0) {
+            if(!isset($requests)) {
+                $errors["noFriendsSelected"] = "You must select at least one friend!";
+            } else {
+                if(empty($errors)) {
+                    foreach($requests as $request) {
+                        acceptFriendRequest($userId, $request);
+                    }
+                }
+            }
+        } else if($submit == 1) {
+            if(!isset($requests)) {
+                $errors["noFriendsSelected"] = "You must select at least one friend!";
+            } else {
+                if(empty($errors)) {
+                    foreach($requests as $request) {
+                        denyFriendRequest($userId, $request);
+                    }
                 }
             }
         }
     }
 ?>
-
 <?php include("./common/header.php"); ?>
 
 <div class="container text-center">
@@ -102,7 +116,7 @@
                     </tbody>
                 </table>
             <div class="d-flex justify-content-end">
-                <button type="submit" value="Submit" name="defriend" class="btn mt-3" style="background-color: DarkSlateBlue; color: white;">Deny Selected</button>
+                <button type="submit" value="Submit" name="defriend" class="btn mt-3" style="background-color: DarkSlateBlue; color: white;">Defriend Selected</button>
             </div>
         </form>
         </div>
@@ -112,7 +126,7 @@
     <div class="card mt-3 mx-auto" style="width: 55rem;">
         <h5 class="card-header text-center">Friend Requests</h5> 
         <div class="card-body">
-        <form action="MyFriends.php" method="post" onsubmit="confirmDeny(event)">
+        <form action="MyFriends.php" method="post">
             <table class="table align-middle mb-0 bg-white">
                 <thead class="bg-light text-start">
                     <tr>
@@ -146,8 +160,8 @@
                 </tbody>
             </table>
             <div class="mt-3 d-flex justify-content-end">
-                <button type="submit" name="acceptRequest" class="btn me-2" style="background-color: DarkSlateBlue; color: white;">Accept Selected</button>
-                <button type="clear" name="clear" class="btn btn-outline-secondary">Deny Selected</button>
+                <button name="submit" value="0" class="btn me-2" id="acceptRequest" style="background-color: DarkSlateBlue; color: white;" onclick="submitWithoutConfirm(this)">Accept Selected</button>
+                <button name="submit" value="1" class="btn btn-outline-secondary" onclick="confirmDeny(event)">Deny Selected</button>
             </div>
         </form>
         </div>
